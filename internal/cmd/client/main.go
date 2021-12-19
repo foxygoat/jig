d9e33854b5f5832e8ae34653bbdc58b90ad0bf19
Julia Ogris <julia.ogris@gmail.com>
2021-12-20 10:47:52 +1100

    Extract Client struct from client command
    Extract Client struct from client command to reuse in server tests.

--- internal/cmd/client/main.go
+++ internal/cmd/client/main.go
@@ -1,19 +1,12 @@
 package main
 
 import (
-	"context"
-	"errors"
 	"fmt"
 	"io"
 	"os"
-	"strings"
 
-	"foxygo.at/jig/pb/greet"
+	"foxygo.at/jig/internal/client"
 	"github.com/alecthomas/kong"
-	"golang.org/x/sync/errgroup"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/metadata"
-	"google.golang.org/grpc/status"
 )
 
 var version = "v0.0.0"
@@ -30,163 +23,22 @@ type config struct {
 func main() {
 	cfg := &config{out: os.Stdout}
 	kctx := kong.Parse(cfg, kong.Vars{"version": version})
-	err := run(cfg)
+	err := kctx.Run()
 	kctx.FatalIfErrorf(err)
 }
 
-func run(cfg *config) error {
-	conn, err := grpc.Dial(cfg.Address, grpc.WithInsecure())
-	if err != nil {
-		return err
-	}
-	defer conn.Close()
-	client := greet.NewGreeterClient(conn)
-	switch cfg.Stream {
-	case "none":
-		err = runUnary(client, cfg)
-	case "client":
-		err = runClientStream(client, cfg)
-	case "server":
-		err = runServerStream(client, cfg)
-	case "bidi":
-		err = runBiDiStream(client, cfg)
-	}
-	return statusWithDetails(err)
-}
-
-func runUnary(client greet.GreeterClient, cfg *config) error {
-	if len(cfg.Names) > 1 {
-		return errors.New("Only one name allowed for unary client requests")
-	}
-	var header, trailer metadata.MD
-	req := &greet.HelloRequest{FirstName: cfg.Names[0]}
-	resp, err := client.Hello(context.Background(), req, grpc.Header(&header), grpc.Trailer(&trailer))
-	fmt.Fprintf(cfg.out, "Header: %v\n", header)
-	if err == nil {
-		_, err = fmt.Fprintf(cfg.out, "Greeting: %s\n", resp.Greeting)
-	}
-	fmt.Fprintf(cfg.out, "Trailer: %v\n", trailer)
-	return err
-}
-
-func runClientStream(client greet.GreeterClient, cfg *config) error {
-	stream, err := client.HelloClientStream(context.Background())
-	if err != nil {
-		return err
-	}
-	for _, name := range cfg.Names {
-		if err := stream.Send(&greet.HelloRequest{FirstName: name}); err != nil {
-			return err
-		}
-	}
-	resp, rerr := stream.CloseAndRecv()
-	header, err := stream.Header()
-	if err != nil {
-		return err
-	}
-	fmt.Fprintf(cfg.out, "Header: %v\n", header)
-	defer fmt.Fprintf(cfg.out, "Trailer: %v\n", stream.Trailer())
-
-	if rerr != nil {
-		return rerr
-	}
-	_, err = fmt.Fprintf(cfg.out, "Greeting: %s\n", resp.Greeting)
-	return err
-}
-
-func runServerStream(client greet.GreeterClient, cfg *config) error {
-	if len(cfg.Names) > 1 {
-		return errors.New("Only one name allowed for unary client requests")
-	}
-	req := &greet.HelloRequest{FirstName: cfg.Names[0]}
-	stream, err := client.HelloServerStream(context.Background(), req)
-	if err != nil {
-		return err
-	}
-
-	header, err := stream.Header()
-	if err != nil {
-		return err
-	}
-	fmt.Fprintf(cfg.out, "Header: %v\n", header)
-	defer fmt.Fprintf(cfg.out, "Trailer: %v\n", stream.Trailer())
-
-	for {
-		resp, err := stream.Recv()
-		if errors.Is(err, io.EOF) {
-			break
-		}
-		if err != nil {
-			return err
-		}
-		_, err = fmt.Fprintf(cfg.out, "Greeting: %s\n", resp.Greeting)
-		if err != nil {
-			return err
-		}
+func (cfg *config) AfterApply() error {
+	if (cfg.Stream == "none" || cfg.Stream == "server") && len(cfg.Names) != 1 {
+		return fmt.Errorf("exactly 1 name required with stream %s", cfg.Stream)
 	}
 	return nil
 }
 
-func runBiDiStream(client greet.GreeterClient, cfg *config) error {
-	errgrp, ctx := errgroup.WithContext(context.Background())
-
-	stream, err := client.HelloBiDiStream(ctx)
+func (cfg *config) Run() error {
+	c, err := client.New(cfg.Address)
 	if err != nil {
 		return err
 	}
-
-	// concurrently run each direction of the stream.
-	errgrp.Go(func() error {
-		for _, name := range cfg.Names {
-			req := &greet.HelloRequest{FirstName: name}
-			if err := stream.Send(req); err != nil {
-				return err
-			}
-		}
-		return stream.CloseSend()
-	})
-	errgrp.Go(func() error {
-		header, err := stream.Header()
-		if err != nil {
-			return err
-		}
-		fmt.Fprintf(cfg.out, "Header: %v\n", header)
-		defer fmt.Fprintf(cfg.out, "Trailer: %v\n", stream.Trailer())
-		for {
-			resp, err := stream.Recv()
-			if errors.Is(err, io.EOF) {
-				return nil
-			}
-			if err != nil {
-				return err
-			}
-			fmt.Fprintf(cfg.out, "Greeting: %s\n", resp.Greeting)
-		}
-	})
-
-	return errgrp.Wait()
-}
-
-func statusWithDetails(err error) error {
-	if st, ok := status.FromError(err); ok && st != nil {
-		return detailStatusErr{st}
-	}
-	return err
-}
-
-type detailStatusErr struct {
-	status *status.Status
-}
-
-func (dst detailStatusErr) Error() string {
-	details := dst.status.Details()
-	if len(details) == 0 {
-		return dst.status.Err().Error()
-	}
-	lines := make([]string, 0, len(details)+1)
-	lines = append(lines, dst.status.Err().Error())
-	for _, d := range details {
-		lines = append(lines, fmt.Sprintf("%v", d))
-	}
-	return strings.Join(lines, "\n")
+	defer c.Close()
+	return c.Call(cfg.out, cfg.Names, cfg.Stream)
 }
